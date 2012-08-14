0c79ac4f29f9ca3c52406a85f3e5043c1e6e87b9
Karim DRIDI <seth.holth@gmail.com>
2012-08-14 21:13:42 +0200

    Ffirst commit!

---
+++ src/main/java/com/arykow/ardor3d/example/Example01.java
@@ -0,0 +1,13 @@
+package com.arykow.ardor3d.example;
+
+import com.ardor3d.example.basic.BoxExample;
+
+public class Example01 extends BoxExample {
+	Example01() {
+		super();
+	}
+
+	public static void main(String[] args) {
+		start(BoxExample.class);
+	}
+}
